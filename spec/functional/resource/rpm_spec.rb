4ebe8ada023ada40420ca2f595723958134acc7c
Prabhu Das <prabhu.das@clogeny.com>
2013-07-22 20:49:31 +0530

    Platform specific package initializations for centos and AIX functional tests.

--- spec/functional/resource/rpm_spec.rb
+++ spec/functional/resource/rpm_spec.rb
@@ -25,33 +25,43 @@ describe Chef::Resource::RpmPackage do
 
   def rpm_pkg_should_be_installed(resource)
     case ohai[:platform]
-
+    # Due to dependency issues , different rpm pkgs are used in different platforms.
+    # glib rpm package works in aix, without any dependency issues.
     when "aix"
-      expect(shell_out("rpm -qa | grep a2ps").exitstatus).to eq(0)
+      expect(shell_out("rpm -qa | grep glib").exitstatus).to eq(0)
+    # hello rpm package works in centos, without any dependency issues.
     when "centos"
-      expect(shell_out("rpm -qa | grep a2ps").exitstatus).to eq(0)
+      expect(shell_out("rpm -qa | grep hello").exitstatus).to eq(0)
     end
   end
 
-
   def rpm_pkg_should_not_be_installed(resource)
     case ohai[:platform]
-
     when "aix"
-      expect(shell_out("rpm -qa | grep a2ps").exitstatus).to eq(0)
+      expect(shell_out("rpm -qa | grep glib").exitstatus).to eq(0)
     when "centos"
-      expect(shell_out("rpm -qa | grep a2ps").exitstatus).to eq(1)
+      expect(shell_out("rpm -qa | grep hello").exitstatus).to eq(1)
     end
   end
 
   before(:each) do
-    FileUtils.cp 'spec/functional/assets/a2ps-4.14-10.1.el6.x86_64.rpm' , "/tmp/a2ps-4.14-10.1.el6.x86_64.rpm"
-    @new_resource = Chef::Resource::RpmPackage.new("a2ps", run_context)
-    @new_resource.source "/tmp/a2ps-4.14-10.1.el6.x86_64.rpm"
+    case ohai[:platform]
+    # Due to dependency issues , different rpm pkgs are used in different platforms.
+    when "aix"
+      FileUtils.cp 'spec/functional/assets/glib-1.2.10-2.aix4.3.ppc.rpm' , "/tmp/glib-1.2.10-2.aix4.3.ppc.rpm"
+      @pkg_name = "glib"
+      @pkg_path = "/tmp/glib-1.2.10-2.aix4.3.ppc.rpm"
+    when "centos"
+      FileUtils.cp 'spec/functional/assets/hello-2.8-1.el6.x86_64.rpm' , "/tmp/hello-2.8-1.el6.x86_64.rpm"
+      @pkg_name = "hello"
+      @pkg_path = "/tmp/hello-2.8-1.el6.x86_64.rpm"
+    end
+    @new_resource = Chef::Resource::RpmPackage.new(@pkg_name, run_context)
+    @new_resource.source @pkg_path
   end
 
   after(:each) do
-    FileUtils.rm "/tmp/a2ps-4.14-10.1.el6.x86_64.rpm"
+    FileUtils.rm @pkg_path
   end
 
   context "package install action" do
